1d64fc75e7705f5da794f8b49d422f285c2963c0
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-10-25 15:48:35 +0200

    cmake improvements,texture/sprite loading&drawing

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Chthonic Metal - v0.0.1");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	sf::Texture texture;
+	if (!texture.loadFromFile("assets/vase.png"))
+		return 1;
+
+	sf::Sprite sprite = sf::Sprite(texture);
+	sprite.setPosition(200, 150);
 
 	while (window.isOpen())
 	{
@@ -16,7 +23,10 @@ int main()
 		}
 
 		window.clear();
+
 		window.draw(shape);
+		window.draw(sprite);
+
 		window.display();
 	}
 
