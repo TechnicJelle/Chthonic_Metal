55b09219a0434bc001fe4d912bd98b46310612f5
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-11-04 14:30:35 +0100

    Optimised the fire. A LOT. 20fps->970fps

--- src/GameObjects/Fire.cpp
+++ src/GameObjects/Fire.cpp
@@ -11,37 +11,40 @@ Fire::Fire(sf::RenderWindow* window) : Engine::GameObject(window, sf::Vector2f(0
 	palette = makePalette();
 //	printPalette(&palette);
 
-	// get from RESOLUTION and maximum of the screen dimensions
-	PIXEL_SIZE = (uint)std::min(window->getSize().x, window->getSize().y) / RESOLUTION;
-	PXL_WIDTH = window->getSize().x / PIXEL_SIZE;
-	PXL_HEIGHT = window->getSize().y / PIXEL_SIZE;
-	PXL_TOTAL = PXL_WIDTH * PXL_HEIGHT;
+	texture.create(W, H);
+	sprite = sf::Sprite(texture);
+	sprite.scale((float)window->getSize().x / (float)W, (float)window->getSize().y / (float)H);
 
-	firePixels = createFireArray();
+	frameBufferInit();
 }
 
 void Fire::draw()
 {
-	GameObject::draw();
-	drawPalette(&palette, window);
-	drawFireArray();
+//	for (int i = 0; i < 100; ++i)
+//		frameBuffer[Random::randi((int)W * (int)H)] = Random::randi((int)palette.size());
+
 	doFire();
+	frameBufferToPixels();
+
+	window->draw(sprite);
+	drawPalette(&palette, window);
 }
 
 std::vector<sf::Color> Fire::makePalette()
 {
 	std::vector<sf::Color> palette;
 
-	for (int i = 0; i < PALETTE_SIZE-1; i++) //-1 to make up for the +1 in the loop
+	for (int i = 0; i < BASE_PALETTE_SIZE - 1; i++) //-1 to make up for the +1 in the loop
 	{
 		sf::Color c1 = colours[i];
 		sf::Color c2 = colours[i + 1];
 		for (int s = 0; s < STEPS_BETWEEN_COLOURS; ++s)
 		{
-			sf::Color c = Utils::lerpColour(c1, c2, (float)(s) / STEPS_BETWEEN_COLOURS);
+			sf::Color c = Utils::lerpColour(c1, c2, (float)s / STEPS_BETWEEN_COLOURS);
 			palette.push_back(c);
 		}
 	}
+	palette.push_back(colours[BASE_PALETTE_SIZE - 1]); //make sure pure white is in the palette
 
 	return palette;
 }
@@ -69,51 +72,48 @@ void Fire::drawPalette(std::vector<sf::Color>* palette, sf::RenderWindow* window
 	}
 }
 
-unsigned int* Fire::createFireArray()
+void Fire::frameBufferInit()
 {
-	unsigned int* array = new unsigned int[PXL_TOTAL];
-	for (int i = 0; i < PXL_TOTAL; i++)
-	{
-		array[i] = 0;
-		if (i >= PXL_TOTAL - PXL_WIDTH)
-		{
-			array[i] = palette.size() - 1;
-		}
-	}
-	return array;
+	//Fill with black
+	for (int x = 0; x < W; x++)
+		for (int y = 0; y < H; y++)
+			frameBuffer[x + y * W] = 0;
+
+	//Set the bottom row to white
+	for (int x = 0; x < W; x++)
+		frameBuffer[x + (H - 1) * W] = (sf::Uint8)palette.size() - 1;
+
 }
 
-void Fire::drawFireArray()
+void Fire::frameBufferToPixels()
 {
-	for (int i = 0; i < PXL_TOTAL; i++)
-	{
-		sf::RectangleShape rect;
-		rect.setPosition(sf::Vector2f(
-				(float)(i % PXL_WIDTH) * (float)PIXEL_SIZE,
-				(float((int)(i / PXL_WIDTH))) * (float)PIXEL_SIZE));
-		rect.setSize(sf::Vector2f((float)PIXEL_SIZE, (float)PIXEL_SIZE));
-		rect.setFillColor(palette.at(firePixels[i]));
-		rect.setOutlineThickness(0);
-		rect.setOutlineColor(sf::Color::Black);
-		window->draw(rect);
-	}
+	for (int x = 0; x < W; x++)
+		for (int y = 0; y < H; y++)
+		{
+			sf::Color c = palette[frameBuffer[x + y * W]];
+			pixels[(x + y * W) * 4 + 0] = c.r;
+			pixels[(x + y * W) * 4 + 1] = c.g;
+			pixels[(x + y * W) * 4 + 2] = c.b;
+			pixels[(x + y * W) * 4 + 3] = 255;
+		}
+
+	texture.update(pixels);
 }
 
 void Fire::doFire()
 {
-	for(unsigned int x = 0 ; x < PXL_WIDTH; x++) {
-		for (unsigned int y = 1; y < PXL_HEIGHT ; y++) {
-			unsigned int colourIndex = firePixels[x + (y * PXL_WIDTH)];
+	for(unsigned int x = 0 ; x < W; x++) {
+		for (unsigned int y = 1; y < H ; y++) {
+			unsigned int colourIndex = frameBuffer[x + (y * W)];
+			unsigned int indexY = y - 1;
 			if(colourIndex) {
 				unsigned int rnd = Random::randi(0, 3);
-				unsigned int indexX = (x - rnd + 1) % PXL_WIDTH;
-				unsigned int indexY = y - 1;
-				firePixels[indexX + (indexY * PXL_WIDTH)] = colourIndex - rnd % 2;
+				unsigned int indexX = (x - rnd + 1) % W;
+				frameBuffer[indexX + (indexY * W)] = colourIndex - rnd % 2;
 			}
 			else {
 				unsigned int indexX = x;
-				unsigned int indexY = y - 1;
-				firePixels[indexX + (indexY * PXL_WIDTH)] = 0;
+				frameBuffer[indexX + (indexY * W)] = 0;
 			}
 		}
 	}
--- src/GameObjects/Fire.hpp
+++ src/GameObjects/Fire.hpp
@@ -6,20 +6,25 @@
 #define CHTHONIC_METAL_FIRE_HPP
 
 
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+
 #include "../Engine/GameObject.hpp"
 
 class Fire : public Engine::GameObject
 {
 private:
 	// Designer variables
-	static const int RESOLUTION = 200;
-	static const int STEPS_BETWEEN_COLOURS = 6; //The higher this value, the higher the flames.
+	const unsigned int W = 256;
+	const unsigned int H = 144;
+	static const int STEPS_BETWEEN_COLOURS = 4; //The higher this value, the higher the flames.
 
+private:
 	// Palette variables
-	static const int PALETTE_SIZE = 5;
-	static inline sf::Color colours[PALETTE_SIZE] = {
+	static const int BASE_PALETTE_SIZE = 5;
+	static inline sf::Color colours[BASE_PALETTE_SIZE] = {
 			sf::Color(0, 0, 0), // Black
-			sf::Color(100, 0, 0), // Red
+			sf::Color(130, 0, 0), // Red
 			sf::Color(180, 80, 0), // Orange
 			sf::Color(255, 255, 0), // Yellow
 			sf::Color(255, 255, 255), // White
@@ -27,13 +32,15 @@ private:
 
 	std::vector<sf::Color> palette;
 
-	// Fire variables
-	unsigned int PIXEL_SIZE = -1;
-	unsigned int PXL_WIDTH = -1;
-	unsigned int PXL_HEIGHT = -1;
-	unsigned int PXL_TOTAL = -1;
+	// Framebuffer variables
+	///index into the palette
+	sf::Uint8* frameBuffer = new sf::Uint8[W * H];
 
-	unsigned int* firePixels;
+	// SFML Image variables
+	///the actual coloured pixels that will be drawn to the screen
+	sf::Uint8* pixels = new sf::Uint8[W*H*4];
+	sf::Texture texture;
+	sf::Sprite sprite;
 
 private:
 	// Palette functions
@@ -43,18 +50,17 @@ private:
 
 	static void drawPalette(std::vector<sf::Color>* palette, sf::RenderWindow* window);
 
-	// Fire functions
-	unsigned int* createFireArray();
-
-	void drawFireArray();
-
-	void doFire();
-
 	// GameObject functions
 public:
 	explicit Fire(sf::RenderWindow* window);
 
 	void draw() override;
+
+	void frameBufferInit();
+
+	void frameBufferToPixels();
+
+	void doFire();
 };
 
 
